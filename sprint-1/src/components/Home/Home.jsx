050a8f1d9ff095e363c6c2f0a98155133b0200d3
Phoebe Lo <phoebeyclo@gmail.com>
2020-09-19 12:30:29 -0700

    Main video added. Data JSON Created

---
+++ sprint-1/src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Player from '../Player/Player';
+
+ const Home = () => {
+    return (
+        <div className="home">
+            <Player />
+        </div>
+    )
+}
+
+export default Home;
--- sprint-1/src/components/Player/Player.jsx
+++ sprint-1/src/components/Player/Player.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 class Player extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            video: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get("./data.json")
+        .then((res) => {
+            this.setState({video: res.data.mainVideo})
+        })
+        .catch(err => console.log(err));
+    }
+
     render() {
         return (
-            <div>
-                
+            <div className="player__container">
+                <div className="player__video-container">
+                    <video 
+                    className="player__video"
+                    poster={this.state.video.image}
+                    src={this.state.video.video}>
+                        <source type="video/mp4" />
+                    </video>
+                </div>
             </div>
         )
     }
