214013f63470351e739ec73cee6aea649314c750
Phoebe Lo <phoebeyclo@gmail.com>
2020-10-04 17:36:03 -0700

    Upload video success

--- sprint-3/client/src/components/Upload/Upload.jsx
+++ sprint-3/client/src/components/Upload/Upload.jsx
@@ -1,39 +1,64 @@
-import React from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
- const Upload = () => {
-    return (
-        <div className="upload">
-            <main className="upload__content container">
-                <div className="upload__heading">
-                    Upload Video
-                </div>
-                <div className="divider upload__divider"></div>
-                <div className="upload__main-content">
-                    <div className="upload__video-preview">
-                        <div className="upload__video-label">
-                            Video thumbnail
-                        </div>
-                        <div className="upload__video-thumbnail">
-                            <img src="/assets/images/upload-video-preview.jpg" alt="Video preview" />
-                        </div>
+const URL = "http://localhost:8080";
+
+class Upload extends Component {
+
+    handleSubmit = (e) =>{
+        e.preventDefault();
+        const videoTitle = e.target.videoTitle.value;
+        const videoDescription = e.target.videoDescription.value;
+
+        axios.post(`${URL}/videos`, {
+            title: videoTitle,
+            description: videoDescription,
+            image: 'https://i.imgur.com/Pr6lP45.jpg'
+        }).then(res => {
+            console.log(res.data);
+        });
+
+        e.target.reset();
+    }
+
+    render() {
+        return (
+            <div className="upload">
+                <main className="upload__content container">
+                    <div className="upload__heading">
+                        Upload Video
                     </div>
-                    <form className="upload__form" action="">
-                        <label className="upload__video-label">Title your video</label>
-                        <input className="upload__form-input input" type="" 
-                            placeholder="Add a title to your video"></input>
-                        <label className="upload__video-label">Add a video description</label>
-                        <textarea className="upload__form-textarea textarea" name="formDescription"
-                            placeholder="Add a description of your video"></textarea>
-                        <div className="divider upload__divider"></div>
-                        <div className="upload__form-control">
-                            <button className="upload__submit-btn btn">Publish</button>
-                            <button className="upload__cancel-btn btn">Cancel</button>
+                    <div className="divider upload__divider"></div>
+                    <div className="upload__main-content">
+                        <div className="upload__video-preview">
+                            <div className="upload__video-label">
+                                Video thumbnail
+                            </div>
+                            <div className="upload__video-thumbnail">
+                                <img src="/assets/images/upload-video-preview.jpg" alt="Video preview" />
+                            </div>
                         </div>
-                    </form>
-                </div>
-            </main>
-        </div>
-    )
+                        <form onSubmit={this.handleSubmit} className="upload__form" action="post">
+                            <label className="upload__video-label">Title your video</label>
+                            <input className="upload__form-input input" 
+                                type="text"
+                                name="videoTitle" 
+                                placeholder="Add a title to your video"></input>
+                            <label className="upload__video-label">Add a video description</label>
+                            <textarea className="upload__form-textarea textarea" 
+                                name="videoDescription"
+                                placeholder="Add a description of your video"></textarea>
+                            <div className="divider upload__divider"></div>
+                            <div className="upload__form-control">
+                                <button className="upload__submit-btn btn">Publish</button>
+                                <button className="upload__cancel-btn btn">Cancel</button>
+                            </div>
+                        </form>
+                    </div>
+                </main>
+            </div>
+        )
+    }
 }
 
 export default Upload;
--- sprint-3/server/routes/video.js
+++ sprint-3/server/routes/video.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const uuid = require("uuid");
+const uuid = require("uuid");
 const router = express.Router();
 const videos = require('../data.json');
 
@@ -15,6 +15,34 @@ router.get("/:id", (req, res) => {
     if (findVideo) {
         res.json(videos.filter((video) => video.id === req.params.id)[0]);
     }
+});
+
+// Post video
+router.post("/", (req, res) => {
+    const newVideo = {
+        id: uuid.v4(),
+        title: req.body.title,
+        channel: 'Bike Channel',
+        image: req.body.image,
+        description: req.body.description,
+        views: "831",
+        likes: "721",
+        duration: "6:12",
+        video: "https://project-2-api.herokuapp.com/stream",
+        timestamp: Date.now(),
+        comments: [
+            {
+                name: "Micheal Lyons",
+                comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of acconcert I have EVER witnessed.",
+                id: "1ab6d9f6-da38-456e-9b09-ab0acd9ce818",
+                likes: 0,
+                timestamp: 1545162149000
+            }
+        ]
+    };
+
+    videos.push(newVideo);
+    res.json(videos);
 });
 
 module.exports = router;
